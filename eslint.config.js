86001f02a205f1cd93faecaa155f0011d247a36b
f3ve <free.eve@pm.me>
2023-10-05 11:45:19 -0800

    inital commit

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import f3veEslintConfig from '@f3ve/eslint-config';
+
+export default [
+  f3veEslintConfig({
+    typescript: true,
+    vue: true,
+    browser: true,
+    unocss: true,
+    prettier: true,
+  }),
+];
